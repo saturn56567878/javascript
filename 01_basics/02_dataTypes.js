17c4299ea64365f896d6f9a0ab4fb994b08745a7
saturn56567878 <saturn56567878@gmail.com>
2025-05-12 01:54:18 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello");//we are using node JS not browser
+// alert(3+3);
+
+console.log(3
+    +       //code readability need to be high
+    3);
+
+console.log("happymon")
+
+let name="happymon"
+let age=18
+let isLoddedIn=false
+//NUMBER=>2 TO POWER 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined =>
+//symbol=> unique
+
+
+//object
+
+console.log(typeof "happy");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
