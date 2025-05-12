15214bc0b5e26d4ecb5327151de92a9144ce1227
saturn56567878 <saturn56567878@gmail.com>
2025-05-12 02:24:59 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,61 @@
+let score="33"
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+
+// score="33abc"
+//  valueInNumber=Number(score)
+//  console.log(typeof(valueInNumber));
+//  console.log(valueInNumber);
+
+//   score=null
+//   valueInNumber=Number(score)
+//   console.log(typeof(valueInNumber));
+//   console.log(valueInNumber);
+
+ score=undefined
+  valueInNumber=Number(score)
+  console.log(typeof(valueInNumber));
+  console.log(valueInNumber);
+
+ score="happy"
+  valueInNumber=Number(score)
+//  console.log(typeof(valueInNumber));
+  console.log(valueInNumber);
+
+//  score=true
+//  valueInNumber=Number(score)
+//  console.log(typeof(valueInNumber));
+//  console.log(valueInNumber);
+
+ //"33"=33
+ //"33abc"= NaN
+ //true=1;false=0
+
+ let isLoggedIn=1
+
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ isLoggedIn=""
+
+  booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+  isLoggedIn="happy"
+
+  booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ //1=true;0=false
+ //""=false
+ //"happy"=true
+
+ let someNumber=33
+ let stringNumber=String(someNumber);
+ console.log(stringNumber)
+
+
+ 
